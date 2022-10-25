e7bf7a62294b00f696392b9c230162ae3adccd28
minjunss <103651022+minjunss@users.noreply.github.com>
2022-10-25 17:08:48 +0900

    Add files via upload

---
+++ Ubit-Auto-Trading/src/test/java/com/example/UbitAutoTrading/UbitAutoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UbitAutoTrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbitAutoTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
